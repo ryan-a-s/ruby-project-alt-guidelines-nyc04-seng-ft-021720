af34d9a688a0a8449b4497903b1631868725c455
96StanleyLee <96stanleylee@gmail.com>
2020-03-05 12:39:31 -0500

    added bunny

--- bin/run.rb
+++ bin/run.rb
@@ -4,5 +4,11 @@ puts `clear`
 puts "====================="
 puts "WELCOME TO LEE-TRADE!"
 puts "====================="
+puts "     ___       "
+puts "   _|___|_     "   
+puts "  　( "+"$".colorize(:green)+"_"+"$".colorize(:green)+")"
+puts " 　┌/つ /￣￣￣/   "
+puts " ￣￣＼/＿＿＿/￣  "
+puts " "
 
 user_cli
